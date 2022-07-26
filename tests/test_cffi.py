797643d823ca7bdfb64e5c59e7f4779d60d34329
synodriver <diguohuangjiajinweijun@gmail.com>
2022-07-26 15:26:32 +0800

    add unitest

--- tests/test_cffi.py
+++ tests/test_cffi.py
@@ -2,11 +2,12 @@
 Copyright (c) 2008-2022 synodriver <synodriver@gmail.com>
 """
 import os
-os.environ["CLHASH_USE_CFFI"] = "1"
 
+os.environ["CLHASH_USE_CFFI"] = "1"
+from random import randint
 from unittest import TestCase
 
-from pyclhash import get_random_seed, hash
+from pyclhash import RANDOM_BYTES_NEEDED_FOR_CLHASH, get_random_seed, hash
 
 
 class TestAll(TestCase):
@@ -20,6 +21,14 @@ class TestAll(TestCase):
         for i in range(1000):
             seed = get_random_seed(1, 2)
             self.assertEqual(hash(b"12", seed), hash(b"12", seed))
+
+    def test_hash_with_key(self):
+        for i in range(1000):
+            seed = bytes(
+                [randint(0, 255) for _ in range(RANDOM_BYTES_NEEDED_FOR_CLHASH)]
+            )
+            self.assertEqual(hash(b"12", seed), hash(b"12", seed))
+
 
 if __name__ == "__main__":
     import unittest
--- tests/test_cy.py
+++ tests/test_cy.py
@@ -1,9 +1,10 @@
 """
 Copyright (c) 2008-2022 synodriver <synodriver@gmail.com>
 """
+from random import randint
 from unittest import TestCase
 
-from pyclhash import get_random_seed, hash
+from pyclhash import RANDOM_BYTES_NEEDED_FOR_CLHASH, get_random_seed, hash
 
 
 class TestAll(TestCase):
@@ -16,6 +17,13 @@ class TestAll(TestCase):
     def test_hash(self):
         for i in range(1000):
             seed = get_random_seed(1, 2)
+            self.assertEqual(hash(b"12", seed), hash(b"12", seed))
+
+    def test_hash_with_key(self):
+        for i in range(1000):
+            seed = bytes(
+                [randint(0, 255) for _ in range(RANDOM_BYTES_NEEDED_FOR_CLHASH)]
+            )
             self.assertEqual(hash(b"12", seed), hash(b"12", seed))
 
 
